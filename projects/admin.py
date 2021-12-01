f91eb87d6b27bee4087409e5412b1a1bf1bc8118
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-12-01 18:15:53 +0500

    review and tag models.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project
+from .models import *
 
 admin.site.register(Project)
+admin.site.register(Review)
+admin.site.register(Tag)
