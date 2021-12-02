336afa02118bb254a6cbc67653d96fd6dc0834b2
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-12-02 23:41:22 +0500

    crud

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from .forms import ProjectForm
+from django.shortcuts import redirect
 
 
 def projects(request):
@@ -18,5 +19,28 @@ def project(request,pk):
 
 def createProject(request):
     form=ProjectForm()
+    if request.method=='POST':
+        print(request.POST)
+        form=ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+
     context={'form':form}
     return render(request,"projects/project_form.html",context)
+def updateProject(request,pk):
+    project =Project.objects.get(id=pk)
+    form=ProjectForm(instance=project)
+
+    if request.method=='POST':
+        print(request.POST)
+        form=ProjectForm(request.POST,instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+
+    context={'form':form}
+    return render(request,"projects/project_form.html",context)
+
