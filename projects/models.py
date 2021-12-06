1395a09b01bdd1858a464b7b6b92ccf77d0e37ff
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-12-06 21:40:05 +0500

    users app

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 import uuid
-
+from users.models import Profile
 # Create your models here.
+
 class Tag(models.Model):
 	name=models.CharField(max_length=200)
 	created=models.DateTimeField(auto_now_add=True)
@@ -12,6 +13,7 @@ class Tag(models.Model):
 		return self.name
 
 class Project(models.Model):
+	owner=models.ForeignKey(Profile,null=True,blank=True,on_delete=models.SET_NULL)
 	title=models.CharField(max_length=200)
 	description=models.TextField(null=True,blank=True)
 	featured_image=models.ImageField(null=True,blank=True,
