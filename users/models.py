366003ceb36109950103375a22b5579b959577b4
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-12-08 13:13:34 +0500

    profile

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
 	user=models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
 	name=models.CharField(max_length=200,null=True,blank=True)
 	username=models.CharField(max_length=200,null=True,blank=True)
+	location=models.CharField(max_length=200,null=True,blank=True)
 	email=models.EmailField(max_length=500, blank=True,null=True)
 
 	short_intro=models.CharField(max_length=300,blank=True,null=True)
@@ -14,6 +15,7 @@ class Profile(models.Model):
 	profile_image=models.ImageField(null=True,blank=True,
 	                                upload_to='profiles/',default="profiles/user-default.png")
 	github=models.CharField(max_length=200,null=True,blank=True)
+	stackoverflow=models.CharField(max_length=200,null=True,blank=True)
 	twitter=models.CharField(max_length=200,null=True,blank=True)
 	linkedin=models.CharField(max_length=200,null=True,blank=True)
 	website=models.CharField(max_length=200,null=True,blank=True)
@@ -22,4 +24,17 @@ class Profile(models.Model):
 	                    primary_key=True,editable=False)
 	def __str__(self):
 		return str(self.user.username)
+
+class Skill(models.Model):
+	owner=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)
+	name=models.CharField(max_length=200,blank=True,null=True)
+	description=models.TextField(null=True,blank=True)
+	created=models.DateTimeField(auto_now_add=True)
+	id = models.UUIDField( default=uuid.uuid4, unique=True,
+	                       primary_key=True, editable=False )
+
+	def __str__(self):
+		return str( self.name )
+
+
 
